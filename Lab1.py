4b6fb6631e78672567004d792015e121e489e269
amohyseneca <amohy@myseneca.ca>
2023-02-15 15:51:31 -0500

    iteration1

--- Lab1.py
+++ Lab1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 name = input("Name: ")
-age = input("Age: ") 
+age = input("Age: ")
+def helloWorld():
+    print("Hello World")
 
+helloWorld()
 print("Hi " + name + ", You are " + str(age) + " years old.")
