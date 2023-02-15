112c40e42f46b462bd9e928beb9c1742ba9876f4
amohyseneca <amohy@myseneca.ca>
2023-02-15 16:42:35 -0500

    initial commit branch

--- Lab1.py
+++ Lab1.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 name = input("Name: ")
-age = input("Age: ")
 def helloWorld():
     print("Hello World")
+
+def calculateAge():
+    age = input("Age: ")
+    try:
+        return int(age)
+    except:
+        print("Please enter an int")
+        continue
+
 
 helloWorld()
 print("Hi " + name + ", You are " + str(age) + " years old.")
